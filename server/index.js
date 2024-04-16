61b1900f667e5f6be767deac7ee9e78004e1f68d
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-17 00:59:36 +0100

    Feat: Installed packages defined in the index file

--- server/index.js
+++ server/index.js
@@ -11,4 +11,13 @@ import {fileURLToPath} from "url"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors())
