565d00d2d49e33340b79a07028b49aae7c9a6333
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-17 00:44:23 +0100

    Feat: Installed packages defined in the index file

--- server/index.js
+++ server/index.js
@@ -2,4 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import multer from "multer"
+import morgan from "morgan";
+import helmet from "helmet";
+import path from "path";
+import {fileURLToPath} from "url"
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
