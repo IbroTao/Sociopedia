83680a89b3115d9309f6b48f036e4c5501cda256
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-17 18:11:38 +0100

    Feat: login & register for users

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrpyt";
+import bcryptjs from "bcryptjs"
 import jwt from "jsonwebtoken";
 import User from "../models/user.models.js"
 
@@ -7,8 +8,8 @@ export const register = async(req, res) => {
     try{
         const {firstName, lastName, email, password, picturePath, location, friends, occupation} = req.body;
 
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password, salt);
+        const salt = await bcryptjs.genSalt();
+        const passwordHash = await bcryptjs.hash(password, salt);
 
         const newUser = new User({
             firstName,
@@ -33,6 +34,10 @@ export const register = async(req, res) => {
 export const login = async(req, res) => {
     try{
         const {email, password} = req.body;
+        const user = await User.findOne({email: email});
+        if(!user) return res.status(400).json({msg: "User does not exist."});
+
+        const isMatch = await bcryptjs.compare(password, user.password);
     }
     catch(err){
         res.status(500).json({error: err.message})
