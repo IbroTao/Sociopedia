7f4d4b250eacf5e1ad56e086957ee300763467e5
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-18 23:18:05 +0100

    Feat: token integrated in user auth

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -40,7 +40,8 @@ export const login = async(req, res) => {
         const isMatch = await bcryptjs.compare(password, user.password);
         if(!isMatch) return res.status(400).json({msg: "Invalid credentials"});
 
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        delete user.password;
     }
     catch(err){  
         res.status(500).json({error: err.message})
