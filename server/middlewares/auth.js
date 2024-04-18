a00058aeaf31f65894384481c1e1f29e2c9872e1
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-19 00:05:55 +0100

    New: user middleware set up before auth

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,6 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = async(req, res, next) => {
     try{
+        let token = req.header("Authorization");
+        if(!token){
+            return res.status(403).send("Access Denied")
+        };
+
+        if(token.startsWith("Bearer ")) {
+            token = token.slice(7, token.length).trimLeft()
+        }
 
     }
     catch(err){
