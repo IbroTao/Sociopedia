774f44499fc1e4368af7c7f25b18dcfd0e3d8405
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-20 14:18:56 +0100

    New: post working phase implementation

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -28,9 +28,10 @@ export const createPost = async(req, res) => {
 
 export const getFeedPosts = async(req, res) => {
     try{
-
+        const post = await Post.find();
+        res.status(200).json(post);
     }
     catch(err){
-        res.status(500).json({error: err.message})
+        res.status(404).json({message: err.message})
     }
 }
