20e3be6437c05548f1ea93d18d9f4f55b4dea98c
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-20 02:43:28 +0100

    New: post working phase implementation

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -1,2 +1,12 @@
-import express from "express";
-import {} from "../controllers"
+import Post from "../models/posts.model";
+import User from "../models/users.model";
+
+export const createPost = async(req, res) => {
+    try{
+        const {userId, picturePath, description} = req.body;
+        const user = await User.findById(userId);
+    }
+    catch(err){
+        res.status(409).json({error: err.message});
+    }
+}
