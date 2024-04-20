08a369b4800595dde274fc75d2d8c1d09a14c796
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-20 14:29:04 +0100

    New: more post functionalities implemented and integrated

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -40,9 +40,26 @@ export const getFeedPosts = async(req, res) => {
 export const getUserPosts = async(req, res) => {
     try{
         const {userId} = req.params;
-        const post = await Post.find({userId})
+        const post = await Post.find({userId});
+        res.status(200).json(post);
     }
     catch(err){
         res.status(404).json({message: err.message});
+    }
+};
+
+export const likePost = async(req, res) => {
+    try{
+        const {id} = req.params;
+        const {userId} = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if(isLiked) {
+            post.likes.delete(userId)
+        }
+    }
+    catch(err){
+        res.status(400).json({message: err.message});
     }
 }
