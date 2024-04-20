06dbdfe79a77783b0cb720120a82b4f225230632
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-20 02:22:50 +0100

    New: post routes set up

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,3 +1,15 @@
 import express from "express";
 import {getFeedPosts, getUserPosts, likePost} from "../controllers/posts.controller";
 import {verifyToken} from "../middlewares/auth.js"
+
+const router = express.Router();
+
+/* READ */
+router.get('/', verifyToken, getFeedPosts);
+router.get('/:userId/posts', verifyToken, getUserPosts)
+
+/* UPDATE */
+router.patch('/:id/like', verifyToken, likePost);
+
+
+export default router;
