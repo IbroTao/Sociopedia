22a9ce577c0eae379fd15a4f67ba523a0889335a
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-20 20:38:58 +0100

    New: user data added

--- server/data/index.js
+++ server/data/index.js
@@ -26,5 +26,20 @@ export const users = [
         createdAt: 11111111,
         updatedAt: 11111111,
         __v: 0,
+    },
+    {
+        _id: userIds[1],
+        firstName: "Patrick",
+        lastName: "Gomes",
+        email: "patgomes@gmail.com",
+        password: "ihhUHU#H*$U*$*$Huh4",
+        picturePath: "p12.jpeg",
+        friends: [],
+        location: "Los Angeles, California",
+        occupation: "Baker",
+        viewedProfile: 1393,
+        impressions: 222222,
+        createdAt: 222222,
+        updatedAt: 222222,
     }
 ]
